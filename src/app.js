c0de1d414480348b398d66c99b32b74079145415
thiaguinhocoding <thiaguinhocoding@gmail.com>
2021-06-05 01:16:23 -0300

    added config for serve static

---
+++ src/app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('dist'));
+
+module.exports = app;
+
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@
 require('dotenv').config();
 const app = require('./app');
 
-
 const SERVER_PORT = process.env.SERVER_PORT || 8080;
 
 app.listen(SERVER_PORT, () => console.log(`Server ready at *:${SERVER_PORT}`));
